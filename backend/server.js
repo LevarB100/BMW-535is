0f7befb132bfdae96d2790e64699488967504375
Levar Barrett <emaillevar@gmail.com>
2020-01-13 11:57:20 -0500

    apdate to server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,30 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const authWare = require("./middleware/authware");
 const ttrackRoutes = express.Router();
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
+​
+const path = require("path");
+​
 let ttrack = require("./models");
-
+​
 app.use(cors());
 app.use(bodyParser.json());
-
+​
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+}
+​
 mongoose.connect("mongodb://127.0.0.1:27017/ttracker", {
   useNewUrlParser: true
 });
 const connection = mongoose.connection;
-
+​
 connection.once("open", function() {
   console.log("Mongodb database connection established succesfully");
 });
-
+​
 require("./routes/api-routes.js")(app);
-
+​
 ttrackRoutes.route("/").get(authWare, function(req, res) {
   console.log(req.user.toObject());
   ttrack.metric.find(
@@ -38,7 +44,7 @@ ttrackRoutes.route("/").get(authWare, function(req, res) {
     }
   );
 });
-
+​
 ttrackRoutes.route("/:id").get(authWare, function(req, res) {
   let id = req.params.id;
   track1.userId = req.user._id;
@@ -46,7 +52,7 @@ ttrackRoutes.route("/:id").get(authWare, function(req, res) {
     res.json(ttrack1);
   });
 });
-
+​
 ttrackRoutes.route("/add").post(authWare, function(req, res) {
   let ttrack1 = req.body;
   console.log(ttrack1);
@@ -60,7 +66,7 @@ ttrackRoutes.route("/add").post(authWare, function(req, res) {
       res.status(400).send("adding new ttrack failed");
     });
 });
-
+​
 ttrackRoutes.route("/update/:id").post(function(req, res) {
   ttrack.metric.findByIdAndUpdate(req.params.id, { $inc: req.body }, function(
     err,
@@ -70,9 +76,15 @@ ttrackRoutes.route("/update/:id").post(function(req, res) {
     res.json("TTrack updated");
   });
 });
-
+​
 app.use("/ttracker", ttrackRoutes);
-
+​
+if (process.env.NODE_ENV === "production") {
+	app.get("*", function(req, res) {
+        res.json(path.join(__dirname, "../client/build/index.html"));
+	});
+}
+​
 app.listen(PORT, function() {
   console.log("server is running brah on PORT" + PORT);
 });
